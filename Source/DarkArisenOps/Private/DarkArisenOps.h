b9cccaaced575e46e2e42aec50cd1d72fdbb1b06
clarityosbaerbelwesterop-gif <clarityos.baerbelwesterop@gmail.com>
2026-08-29 19:34:57 +0200

    ci: expose native boss validator

--- Source/DarkArisenOps/Private/DarkArisenOps.h
+++ Source/DarkArisenOps/Private/DarkArisenOps.h
@@ -48,6 +48,7 @@ FString OpsBinaryPath(const FString& Root);
 int32 ValidateCommand(const FParsedArgs& Args);
 int32 ValidateDungeonContentCommand(const FParsedArgs& Args);
 int32 ValidateMissionContentCommand(const FParsedArgs& Args);
+int32 ValidateBossContentCommand(const FParsedArgs& Args);
 int32 RunnerCheckCommand(const FParsedArgs& Args);
 int32 BuildCommand(const FParsedArgs& Args);
 int32 PackageAlphaCommand(const FParsedArgs& Args);
