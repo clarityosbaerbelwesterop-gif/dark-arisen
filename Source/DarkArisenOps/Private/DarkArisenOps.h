31c66974f87b0e84d5776c5f5bdba950e43a1cd7
clarityosbaerbelwesterop-gif <clarityos.baerbelwesterop@gmail.com>
2026-08-29 20:04:15 +0200

    ci: expose world presentation and audio voice validators

--- Source/DarkArisenOps/Private/DarkArisenOps.h
+++ Source/DarkArisenOps/Private/DarkArisenOps.h
@@ -50,6 +50,8 @@ int32 ValidateDungeonContentCommand(const FParsedArgs& Args);
 int32 ValidateMissionContentCommand(const FParsedArgs& Args);
 int32 ValidateBossContentCommand(const FParsedArgs& Args);
 int32 ValidateWorldContentCommand(const FParsedArgs& Args);
+int32 ValidatePresentationContentCommand(const FParsedArgs& Args);
+int32 ValidateAudioVoiceContentCommand(const FParsedArgs& Args);
 int32 RunnerCheckCommand(const FParsedArgs& Args);
 int32 BuildCommand(const FParsedArgs& Args);
 int32 PackageAlphaCommand(const FParsedArgs& Args);
