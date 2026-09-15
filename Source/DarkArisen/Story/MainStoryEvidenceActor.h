1943009ca28bbac4c5520a282389f83e17a6dbae
clarityosbaerbelwesterop-gif <clarityos.baerbelwesterop@gmail.com>
2026-09-15 15:46:09 +0200

    feat(story): allow physical evidence to require a defeated holder

--- Source/DarkArisen/Story/MainStoryEvidenceActor.h
+++ Source/DarkArisen/Story/MainStoryEvidenceActor.h
@@ -11,6 +11,8 @@ public:
     UPROPERTY(EditAnywhere, BlueprintReadOnly) FName MissionId;
     UPROPERTY(EditAnywhere, BlueprintReadOnly) FName CheckpointId;
     UPROPERTY(EditAnywhere, BlueprintReadOnly) FName SpawnId;
+    /** Optional fail-closed gate for evidence physically secured behind a Holder encounter. */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly) FName RequiredBossId;
     virtual bool CanInteract_Implementation(AActor* Interactor) const override;
     virtual void CompleteInteraction_Implementation(AActor* Interactor) override;
 };
