21fcabf711cbf6dc360e86fd945d0080c81dda74
clarityosbaerbelwesterop-gif <clarityos.baerbelwesterop@gmail.com>
2026-09-18 22:25:07 +0200

    feat(menu): expose complete native front-end controls

--- Source/DarkArisen/UI/AlphaMenuPlayerController.h
+++ Source/DarkArisen/UI/AlphaMenuPlayerController.h
@@ -2,14 +2,21 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "AlphaMenuPlayerController.generated.h"
-UCLASS() class DARKARISEN_API AAlphaMenuPlayerController : public APlayerController
+UENUM(BlueprintType) enum class EAlphaMenuPage:uint8{Main,Settings};
+UCLASS() class DARKARISEN_API AAlphaMenuPlayerController:public APlayerController
 {
     GENERATED_BODY()
 public:
     virtual void BeginPlay() override;
+    UPROPERTY(BlueprintReadOnly,Category="Alpha|Menu") EAlphaMenuPage MenuPage=EAlphaMenuPage::Main;
     UFUNCTION(BlueprintCallable,Category="Alpha|Menu") bool StartNewGame();
     UFUNCTION(BlueprintCallable,Category="Alpha|Menu") bool ContinueGame();
+    UFUNCTION(BlueprintCallable,Category="Alpha|Menu") void OpenSettings();
+    UFUNCTION(BlueprintCallable,Category="Alpha|Menu") void BackToMain();
     UFUNCTION(BlueprintCallable,Category="Alpha|Menu") void SetVSyncEnabled(bool bEnabled);
+    UFUNCTION(BlueprintCallable,Category="Alpha|Menu") void ToggleVSync();
+    UFUNCTION(BlueprintCallable,Category="Alpha|Menu") void SetQualityPreset(int32 Level);
+    UFUNCTION(BlueprintCallable,Category="Alpha|Menu") void ToggleFullscreen();
     UFUNCTION(BlueprintPure,Category="Alpha|Menu") bool HasContinueSave() const;
     UFUNCTION(BlueprintCallable,Category="Alpha|Menu") void QuitGame();
     static const FString& GetAlphaSaveSlot();
