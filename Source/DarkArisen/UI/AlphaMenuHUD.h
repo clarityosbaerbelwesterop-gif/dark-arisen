4b302e7d755d7a2b1c8941fac8968c91c40a7952
clarityosbaerbelwesterop-gif <clarityos.baerbelwesterop@gmail.com>
2026-09-18 22:25:22 +0200

    feat(menu): add clickable native alpha menu

--- Source/DarkArisen/UI/AlphaMenuHUD.h
+++ Source/DarkArisen/UI/AlphaMenuHUD.h
@@ -2,9 +2,12 @@
 #include "CoreMinimal.h"
 #include "GameFramework/HUD.h"
 #include "AlphaMenuHUD.generated.h"
-UCLASS() class DARKARISEN_API AAlphaMenuHUD : public AHUD
+UCLASS() class DARKARISEN_API AAlphaMenuHUD:public AHUD
 {
     GENERATED_BODY()
 public:
     virtual void DrawHUD() override;
+    virtual void NotifyHitBoxClick(FName BoxName) override;
+private:
+    void DrawButton(const FString& Label,FName Id,float X,float Y,float W,float H,bool bEnabled=true);
 };
